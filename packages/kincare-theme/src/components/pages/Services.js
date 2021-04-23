2fbd7013989b99ed611184eccc974dcde5880098
sam <sam@localhost.localdomain>
2021-04-23 11:11:58 -0400

    refactor using repeater fields

--- packages/kincare-theme/src/components/pages/Services.js
+++ packages/kincare-theme/src/components/pages/Services.js
@@ -3,26 +3,7 @@ import { connect, styled } from 'frontity'
 
 const Services = ({ state, page }) => {
 
-    const { title, subtitle, service_1, service_2, service_3, service_4, service_5, service_6 } = page.acf
-
-    if(service_1.bullets) {
-        service_1.formattedBullets = service_1.bullets.split(',')
-    }
-    if(service_2.bullets) {
-        service_2.formattedBullets = service_2.bullets.split(',')
-    }
-    if(service_3.bullets) {
-        service_3.formattedBullets = service_3.bullets.split(',')
-    }
-    if(service_4.bullets) {
-        service_4.formattedBullets = service_4.bullets.split(',')
-    }
-    if(service_5.bullets) {
-        service_5.formattedBullets = service_5.bullets.split(',')
-    }
-    if(service_6.bullets) {
-        service_6.formattedBullets = service_6.bullets.split(',')
-    }
+    const { title, subtitle, services } = page.acf
 
     const Content = styled.section`
         background-color:${state.theme.colors.lightGrayBlue};
@@ -94,76 +75,24 @@ const Services = ({ state, page }) => {
                     </div>
                 </div>
                 <div className="row justify-content-center">
-                    <div className="col col-12 col-md-6 pb-4">
-                        <Service>
-                            <div className="cover-image" style={{backgroundImage: `url(${service_1.image})` }}></div>
-                            <h3>{service_1.name}</h3>
-                            <p>{service_1.description}</p>
-                            <ul>
-                                {service_1.formattedBullets.map(point => {
-                                    return (
-                                        <li>{point}</li>
-                                    )
-                                })}
-                            </ul>
-                        </Service>
-                    </div>
-                    <div className="col col-12 col-md-6 pb-4">
-                        <Service>
-                            <div className="cover-image" style={{backgroundImage: `url(${service_2.image})` }}></div>
-                            <h3>{service_2.name}</h3>
-                            <p>{service_2.description}</p>
-                            <ul>
-                                {service_2.formattedBullets.map(point => {
-                                    return (
-                                        <li>{point}</li>
-                                    )
-                                })}
-                            </ul>
-                        </Service>
-                    </div>
-                    <div className="col col-12 col-md-6 pb-4">
-                        <Service>
-                            <div className="cover-image" style={{backgroundImage: `url(${service_3.image})` }}></div>
-                            <h3>{service_3.name}</h3>
-                            <p>{service_3.description}</p>
-                            <ul>
-                                {service_3.formattedBullets.map(point => {
-                                    return (
-                                        <li>{point}</li>
-                                    )
-                                })}
-                            </ul>
-                        </Service>
-                    </div>
-                    <div className="col col-12 col-md-6 pb-4">
-                        <Service>
-                            <div className="cover-image" style={{backgroundImage: `url(${service_4.image})` }}></div>
-                            <h3>{service_4.name}</h3>
-                            <p>{service_4.description}</p>
-                            <ul>
-                                {service_4.formattedBullets.map(point => {
-                                    return (
-                                        <li>{point}</li>
-                                    )
-                                })}
-                            </ul>
-                        </Service>
-                    </div>
-                    <div className="col col-12 col-md-6 pb-4">
-                        <Service>
-                            <div className="cover-image" style={{backgroundImage: `url(${service_5.image})` }}></div>
-                            <h3>{service_5.name}</h3>
-                            <p>{service_5.description}</p>
-                            <ul>
-                                {service_5.formattedBullets.map(point => {
-                                    return (
-                                        <li>{point}</li>
-                                    )
-                                })}
-                            </ul>
-                        </Service>
-                    </div>
+                    {Object.values(services).map(service => {
+                        return (
+                            <div className="col col-12 col-md-6 pb-4" key={service.name}>
+                                <Service>
+                                    <div className="cover-image" style={{backgroundImage: `url(${service.image})` }}></div>
+                                    <h3>{service.name}</h3>
+                                    <p>{service.description}</p>
+                                    <ul>
+                                        {Object.values(service.bullets).map((point, index) => {
+                                            return (
+                                                <li key={index}>{point.value}</li>
+                                            )
+                                        })}
+                                    </ul>
+                                </Service>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         </Content>
